1c201344ced6a2050661b0451d440b6372b8e27c
Arnab Ghosh <arnab.hetc18@gmail.com>
2022-03-08 03:03:12 +0530

    added comment in split_data_set.py

--- src/SplitingDataSet/split_data_set.py
+++ src/SplitingDataSet/split_data_set.py
@@ -3,27 +3,42 @@ import splitfolders
 import os
 
 
+#input dataset path 
+input_dataset_folder = ""
 
-input_dataset_folder = r"F:\Study\PGDAIML\Project-10\Latest_data_set\NewDataSet-Cropped-Light"
-
-processed_destination_folder= r"F:\Study\PGDAIML\Project-10\Latest_data_set\Processed_data\Processed_data_from_colorLite_dataset-70-15-15"
+#splitted dataset path 
+processed_destination_folder= ""
 
 
 def get_recursive_file_count(path):
+    
+    """
+    This method counts the number of file recursively on given path
+
+    """
+
     noOfFiles = 0
     noOfDir = 0
     
-    for base, dirs, files in os.walk(path):
-        print('Looking in : ',base)
-        for directories in dirs:
-            noOfDir += 1
-        for Files in files:
-            noOfFiles += 1
-    
-    return noOfFiles
+    try :
+        for base, dirs, files in os.walk(path):
+            print('Looking in : ',base)
+            for directories in dirs:
+                noOfDir += 1
+            for Files in files:
+                noOfFiles += 1
+        
+        return noOfFiles
+   
+    except Exception as err :
+        print("Exception caught in get_recursive_file_count , the exception is {} ".format(err))
 
 def validate_spliting():    
 
+    """
+    This method validate the spliting done by the lib 'splitfolders' 
+
+    """
     number_of_image_per_class = {}
     image_distributuion_class = {}
 
@@ -44,6 +59,6 @@ def validate_spliting():
 
 
 
-#splitfolders.ratio(input=input_dataset_folder,output=processed_destination_folder,seed=42,ratio=(0.7,0.15,0.15))
+splitfolders.ratio(input=input_dataset_folder,output=processed_destination_folder,seed=42,ratio=(0.7,0.15,0.15)) # this method splits the dataset into part 
 
 validate_spliting()
